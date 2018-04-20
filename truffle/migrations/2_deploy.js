6901bcde054a858078ebefc3e5e6a85e992211eb
Adi Kancherla <adikancherla@google.com>
2018-04-19 18:18:54 -0700

    second

--- truffle/migrations/2_deploy.js
+++ truffle/migrations/2_deploy.js
@@ -1,5 +1,7 @@
 var Picolo = artifacts.require("Picolo");
+var KittyCore = artifacts.require("KittyCore");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(Picolo);
+  deployer.deploy(KittyCore);
 };
